8b12339b9d617494dd52b0b810af1739d177c5a6
Fahim Alif <fahimalif077@gmail.com>
2025-05-27 16:33:17 +0600

    change db connection settings

--- db/index.js
+++ db/index.js
@@ -1,10 +1,21 @@
 
 import {Sequelize} from 'sequelize'
-import { DATABASE_URL } from '../constants.js';
+import {  DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME } from '../constants.js';
 
+console.log(DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USERNAME);
 
+const mysql2 = await import('mysql2');
 
-export const sequelize = new Sequelize(DATABASE_URL) 
+export const sequelize = new Sequelize({
+  host:DB_HOST,
+  username:DB_USERNAME,
+  password:DB_PASSWORD,
+  database: DB_NAME,
+  port:DB_PORT,
+  dialect: 'mysql',
+  dialectModule:mysql2,
+  benchmark: true,
+}) 
 const connectDB = async () => {
   try {
      await sequelize.authenticate();
